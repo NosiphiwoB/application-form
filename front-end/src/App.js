25e2a0f78484e0a286e91363292a69599102be45
NosiphiwoB <biyelanosiphiwo@gmail.com>
2022-12-12 15:16:49 +0200

    added a form

---
+++ front-end/src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Application Form</h1>
+
+      <div>
+        <label>First Name</label>
+        <input />
+      </div>
+
+      <div>
+        <label>Surname</label>
+        <input />
+      </div>
+ 
+      <div>
+       <label>Phone</label>
+       <input />
+      </div>
+
+      <div>
+        <label></label>
+      </div>
+
+      <div>
+        <label>Email</label>
+        <input />
+      </div>
+
+     <div>
+      <label>Age</label>
+      <input />
+     </div>
+   
+     <div>
+      <button>Submit</button>
+     </div>
+
+    </div>
+  );
+}
+
+export default App;
